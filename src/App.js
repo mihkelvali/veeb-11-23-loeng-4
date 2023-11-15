82a777a6b954962f620083eac7566e36947b1ea2
mihkelvali <mihkel.vali@hotmail.com>
2023-11-15 19:47:55 +0200

    loeng 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Checkbox from '@mui/material/Checkbox';
+
 import './App.css';
 
+/*
+{
+  text: 'pese hambad',
+  isChecked: false,
+  id: 0,
+  isDeleted: false
+}
+*/
+
 function App() {
+  const [todoIdCounter, setTodoIdCounter] = useState(0);
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState([
+    {
+      text: 'pese hambad',
+      isChecked: false,
+      id: 0
+    }
+  ]);
+
+
+
+  const addNewTodo = () => {
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      {
+        text: inputValue,
+        isChecked: false,
+        isDeleted: false,
+        id: todos.length
+      }
+    ]);
+    setInputValue('');
+    console.log(todos);
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h2>todos</h2>
+        <Box
+          sx={{
+            maxWidth: 400,
+            width: "100%"
+          }}
         >
-          Learn React
-        </a>
+          <Stack spacing={2} direction="row">
+            <TextField
+              fullWidth
+              label="What needs to be done?"
+              variant="standard"
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyDown={(ev) => {
+                if (ev.key === 'Enter') {
+                  addNewTodo();
+                  ev.preventDefault();
+                }
+              }}
+            />
+            <Button variant="outlined" onClick={addNewTodo}>Add</Button>
+          </Stack>
+          {/* todo list */}
+          {
+            todos.map((todo) => (
+              !todo.isDeleted ? (
+                <div key={todo.id}>
+                  <Checkbox checked={todo.isChecked} /> {todo.text} <a>x</a>
+                </div>
+              ) : null
+            ))
+          }
+        </Box>
       </header>
     </div>
   );
